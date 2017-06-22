af57434a83c3e81274d8d0ab3b622fd6ac992336
nljb <a@nljb.net>
2017-06-22 22:14:30 +0800

    update ...

--- goutil_test.go
+++ goutil_test.go
@@ -2,8 +2,63 @@ package goutil
 
 import (
 	"testing"
+	"unicode/utf8"
 )
 
-func TestMain(t *testing.T) {
+func TestString2Float(t *testing.T) {
+	v := String2Float("3.1415926", -1)
+	if v != 3.1415926 {
+		t.Fail()
+	}
+}
 
+func TestString2Int(t *testing.T) {
+	v := String2Int("123", -1)
+	if v != 123 {
+		t.Fail()
+	}
+}
+
+func TestString2Int64(t *testing.T) {
+	v := String2Int64("123456", -1)
+	if v != 123456 {
+		t.Fail()
+	}
+}
+
+func TestString2Utf8(t *testing.T) {
+	v := String2Utf8("你好")
+	if !utf8.Valid([]byte(v)) {
+		t.Fail()
+	}
+}
+
+func TestIsSpace(t *testing.T) {
+	if IsSpace('a') {
+		t.Fail()
+	}
+	if !IsSpace(' ') {
+		t.Fail()
+	}
+	if !IsSpace('\n') {
+		t.Fail()
+	}
+	if !IsSpace('\t') {
+		t.Fail()
+	}
+	if !IsSpace('\r') {
+		t.Fail()
+	}
+}
+
+func TestIsBlank(t *testing.T) {
+	if !IsBlank(" ") {
+		t.Fail()
+	}
+	if !IsBlank("\t\n\r") {
+		t.Fail()
+	}
+	if IsBlank("abc") {
+		t.Fail()
+	}
 }
